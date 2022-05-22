7e8b5c3217279d28521079adfaf71f7c89571b4a
Erez Rokah <erezrokah@users.noreply.github.com>
2022-05-22 10:19:52 +0300

    Initial commit

---
+++ client/config.go
@@ -0,0 +1,31 @@
+package client
+
+// Provider Configuration
+
+type ResourceConfig struct {
+	Name  string
+	Other map[string]interface{} `hcl:",inline"`
+}
+
+type Config struct {
+	// here goes top level configuration for your provider
+	// This object will be pass filled in depending on user's configuration
+	// CHANGEME
+	ExampleConfig string `hcl:"example_config"`
+
+	// resources that user asked to fetch
+	// each resource can have optional additional configurations
+	Resources []ResourceConfig
+}
+
+func (Config) Example() string {
+	return `configuration {
+	// CHANGEME:
+	//Here you define your default/example documentation.
+	//That is generated with cloudquery init YourProviderName
+	// Optional or required parameters
+	// debug = false
+	// api_key = ""	
+}
+`
+}
