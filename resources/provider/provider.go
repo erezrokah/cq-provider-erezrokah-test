633f3e481f84d9943076b92712c888e1d6901846
erezrokah <erezrokah@users.noreply.github.com>
2022-05-22 11:58:30 +0300

    fix: Rename provider

--- resources/provider/provider.go
+++ resources/provider/provider.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/cloudquery/cq-provider-sdk/provider"
 	"github.com/cloudquery/cq-provider-sdk/provider/schema"
+
 	// CHANGEME: change the following to your own package
 	"github.com/cloudquery/cq-provider-template/client"
 	"github.com/cloudquery/cq-provider-template/resources/services/demo"
@@ -20,7 +21,7 @@ func Provider() *provider.Provider {
 	return &provider.Provider{
 		Version: Version,
 		// CHANGEME: Change to your provider name
-		Name:      "YourProviderName",
+		Name:      "erezrokah-test",
 		Configure: client.Configure,
 		ResourceMap: map[string]*schema.Table{
 			// CHANGEME: place here all supported resources
